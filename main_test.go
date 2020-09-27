f18176abd9e26d3ba1231fd12069e570f58430e2
wade <wade-liwei@icloud.com>
2020-09-27 11:00:38 +0800

    unit test

---
+++ main_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestValidatorsByHeight(t *testing.T) {
+
+	height := 79820
+
+	cfg := NodeConfig{
+		RPCNode:           "tcp://192.168.3.200:26657",
+		APIServerEndpoint: "http://192.168.3.100:8545",
+	}
+
+	cli := NewClient(&cfg)
+
+	valSet, err := cli.ValidatorSet(int64(height))
+	if err != nil {
+		t.Errorf("validatorset  error: %v \n", err.Error())
+	}
+
+	jsonValSetAsJson, err := json.MarshalIndent(valSet, "", "\t")
+	if err != nil {
+		t.Errorf("validator json marshal err:  %v \n", err.Error())
+	}
+
+	t.Logf("height:  %v  \n   %s \n", height, jsonValSetAsJson)
+
+}
